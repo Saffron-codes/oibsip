ebc82a465c96bfdcbaf0d9bd163b6e59af7ff3b4
Saffron-codes <saffrontdionysius@gmail.com>
2023-04-30 12:53:59 +0530

    Completed Calculator

--- Calculator/app/src/main/java/com/sddev/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/sddev/calculator/MainActivity.java
@@ -7,64 +7,78 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-    String calc = "";
-    TextView calcTV;
+    String calc = "",calculations = "";
+    TextView calcTV,resultsTV;
     double firstNum,secondNum,result = 0;
     char operator = ' ';
+    boolean canAddOperator = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calcTV = findViewById(R.id.calcTV);
+        calcTV = findViewById(R.id.calculationTV);
+        resultsTV = findViewById(R.id.resultsTV);
     }
 
     public void clearCalc(View view){
         calc = "";
-        calcTV.setText(calc);
+        calculations = calc;
+        calcTV.setText(calculations);
+        resultsTV.setText("");
     }
 
     public void numClicked(View view) {
         Button button = (Button) view;
         calc = calc.concat(button.getText().toString());
-        calcTV.setText(calc);
+        calculations = calculations.concat(button.getText().toString());
+        calcTV.setText(calculations);
+        canAddOperator = true;
     }
 
     public void operatorClicked(View view) {
         Button button = (Button) view;
-        operator = button.getText().charAt(0);
-        firstNum = Double.parseDouble(calc);
-        calc = "";
-        calcTV.setText(button.getText());
-        //calcTV.setText();
+        if(canAddOperator){
+            operator = button.getText().charAt(0);
+            firstNum = Double.parseDouble(calc);
+            calc = calc.concat(button.getText().toString());
+            calculations = calc;
+            calcTV.setText(calculations);
+            canAddOperator = false;
+            calc = "";
+        }
     }
 
     public void equalClicked(View view){
         secondNum = Double.parseDouble(calc);
+
         switch (operator){
             case '+':
                 result = firstNum + secondNum;
-                calcTV.setText(String.valueOf(result));
+                resultsTV.setText(String.valueOf(result));
                 break;
             case '-':
                 result = firstNum - secondNum;
-                calcTV.setText(String.valueOf(result));
+                resultsTV.setText(String.valueOf(result));
                 break;
             case '/':
                 result = firstNum / secondNum;
-                calcTV.setText(String.valueOf(result));
+                resultsTV.setText(String.valueOf(result));
                 break;
-            case 'X':
+            case 'x':
                 result = firstNum * secondNum;
-                calcTV.setText(String.valueOf(result));
-                break;
-            default:
+                resultsTV.setText(String.valueOf(result));
                 break;
         }
+
     }
 
     public void removeClicked(View view) {
-        calc = calc.substring(0,calc.length()-1);
-        calcTV.setText(calc);
+        if(!calc.isEmpty()) {
+            calc = calc.substring(0, calc.length() - 1);
+            calculations = calc;
+            calcTV.setText(calculations);
+        }
     }
 }
